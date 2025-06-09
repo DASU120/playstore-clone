5c386b762c414984d2362713ec7f5d6888f13cc7
Dasu Vasanthati <vasanthatidasu120@gmail.com>
2025-06-09 19:32:36 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const TabItem = props => {
-  const {tabDetails, setActiveTabId, isActive} = props
-  const {tabId, displayText} = tabDetails
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-  const onClickTab = () => {
-    setActiveTabId(tabId)
-  }
-
-  const tabBtnClassName = isActive ? 'tab-button active' : 'tab-button'
-
-  return (
-    <li className="tab-item">
-      <button type="button" onClick={onClickTab} className={tabBtnClassName}>
-        {displayText}
-      </button>
-    </li>
-  )
-}
-
-export default TabItem
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
